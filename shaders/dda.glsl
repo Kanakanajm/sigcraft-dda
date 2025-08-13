251432461f6d41438eefde5f4d5ea2d0ba532afd
Daviti Petriashvili <59766692+memory-hunter@users.noreply.github.com>
2025-08-13 16:04:07 +0200

    shader doesn't die anymore

--- shaders/dda.glsl
+++ shaders/dda.glsl
@@ -4,7 +4,7 @@
 #extension GL_EXT_buffer_reference : require
 
 #define M_PI 3.14159265358979323846
-#define VISIBILITY 500 // maximum number of blocks to traverse in DDA
+#define VISIBILITY 100 // maximum number of blocks to traverse in DDA
 
 layout(set = 0, binding = 0) uniform image2D renderTarget;
 
@@ -22,34 +22,34 @@ push_constants;
 
 void main() {
   ivec2 img_size = imageSize(renderTarget);
-  // float img_aspect_ratio = img_size.x / float(img_size.y);
+  float img_aspect_ratio = img_size.x / float(img_size.y);
 
-  // // spawn camera
-  // vec4 camera_origin = vec4(0, 0, 0, 1);
-  // float px = (2 * ((gl_GlobalInvocationID.x + 0.5) / img_size.x) - 1) *
-  //            tan(push_constants.fov / 2 * M_PI / 180) * img_aspect_ratio;
-  // float py = (1 - 2 * ((gl_GlobalInvocationID.y + 0.5) / img_size.y)) *
-  //            tan(push_constants.fov / 2 * M_PI / 180);
-  // vec3 camera_dir_ = normalize(vec3(px, py, -1));
-  // vec4 camera_dir = vec4(camera_dir_.x, camera_dir_.y, camera_dir_.z, 0);
+  // spawn camera
+  vec4 camera_origin = vec4(0, 0, 0, 1);
+  float px = (2 * ((gl_GlobalInvocationID.x + 0.5) / img_size.x) - 1) *
+             tan(push_constants.fov / 2 * M_PI / 180) * img_aspect_ratio;
+  float py = (1 - 2 * ((gl_GlobalInvocationID.y + 0.5) / img_size.y)) *
+             tan(push_constants.fov / 2 * M_PI / 180);
+  vec3 camera_dir_ = normalize(vec3(px, py, -1));
+  vec4 camera_dir = vec4(camera_dir_.x, camera_dir_.y, camera_dir_.z, 0);
 
-  // // camera space to world space
-  // camera_dir = push_constants.m * camera_dir;
-  // camera_origin = push_constants.m * camera_origin;
+  // camera space to world space
+  camera_dir = push_constants.m * camera_dir;
+  camera_origin = push_constants.m * camera_origin;
 
-  // // world space to chunk space
-  // camera_origin.x -= push_constants.chunk_pos.x;
-  // camera_origin.z -= push_constants.chunk_pos.y;
+  // world space to chunk space
+  camera_origin.x -= push_constants.chunk_pos.x;
+  camera_origin.z -= push_constants.chunk_pos.y;
 
-  vec3 camera_origin = vec3(16, 384, 16);
-  vec3 dir = vec3(0, -1, 0);
+  // vec3 camera_origin = vec3(16, 384, 16);
+  // vec3 dir = vec3(0, -1, 0);
 
-  vec3 planeX = vec3(-12, 0, 0);
-  vec3 planeY = vec3(0, 0, -8);
-  float cameraX = 2 * gl_GlobalInvocationID.x / float(img_size.x) - 1;
-  float cameraY = 2 * gl_GlobalInvocationID.y / float(img_size.y) - 1;
+  // vec3 planeX = vec3(-12, 0, 0);
+  // vec3 planeY = vec3(0, 0, -8);
+  // float cameraX = 2 * gl_GlobalInvocationID.x / float(img_size.x) - 1;
+  // float cameraY = 2 * gl_GlobalInvocationID.y / float(img_size.y) - 1;
 
-  vec3 camera_dir = dir + planeX * cameraX + planeY * cameraY;
+  // vec3 camera_dir = dir + planeX * cameraX + planeY * cameraY;
 
   // block on map
   int mapX = int(camera_origin.x);
@@ -125,25 +125,28 @@ void main() {
     }
 
     // Check if ray has hit a wall
-    if (push_constants.chunk_buffer.data[mapX][mapY][mapZ] > 0) {
-      hit = 1;
-      block_type = push_constants.chunk_buffer.data[mapX][mapY][mapZ];
+    if (mapX >= 0 && mapX < 16 && mapZ >= 0 && mapZ < 16 && mapY >= 0 && mapY < 384 ) {
+      if (push_constants.chunk_buffer.data[mapX][mapY][mapZ] > 0) {
+        hit = 1;
+        block_type = push_constants.chunk_buffer.data[mapX][mapY][mapZ];
+      }
     }
     i++;
   }
-  vec4 c = vec4(1.0, 0.0, 0.0, 1.0);
 
-  if (hit == 1) {
-    c = vec4(1.0, 1.0, 1.0, 1.0);
-  }
+  vec4 c = vec4(0.0, 0.0, 0.0, 1.0);
 
-  // if (block_type > 0 && block_type < 14) {
-  //   // color known block type
-
-  //   c.x = push_constants.color_palette[block_type].x;
-  //   c.y = push_constants.color_palette[block_type].y;
-  //   c.z = push_constants.color_palette[block_type].z;
+  // if (hit == 1) {
+  //   c = vec4(1.0, 1.0, 1.0, 1.0);
   // }
+
+  if (block_type > 0 && block_type < 14) {
+    // color known block type
+
+    c.x = push_constants.color_palette[block_type].x;
+    c.y = push_constants.color_palette[block_type].y;
+    c.z = push_constants.color_palette[block_type].z;
+  }
 
   imageStore(renderTarget, ivec2(gl_GlobalInvocationID.xy), c);
 }
